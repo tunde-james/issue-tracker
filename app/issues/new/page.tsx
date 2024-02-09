c7fed46452a6b9e6bf1fd5f24a65f8027829dab2
Tunde James <tundejames04@gmail.com>
2024-02-09 09:50:46 +0100

    Show the issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,7 +3,7 @@
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import axios from 'axios';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import { useForm, Controller } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';

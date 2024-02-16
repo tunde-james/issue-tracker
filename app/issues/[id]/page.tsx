b35f44d76285ae6b2c29744b03f25b8c7bb472a0
Tunde James <tundejames04@gmail.com>
2024-02-16 09:09:24 +0100

    Secure the application API endpoints

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,15 +1,19 @@
-import { Box, Grid, Flex } from '@radix-ui/themes';
+import authOptions from '@/app/auth/authOptions';
+import prisma from '@/prisma/client';
+import { Box, Flex, Grid } from '@radix-ui/themes';
+import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
+import DeleteIssueButton from './DeleteIssueButton';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
-import prisma from '@/prisma/client';
-import DeleteIssueButton from './DeleteIssueButton';
 
 interface Props {
   params: { id: string };
 }
 
 async function IssueDetailpage({ params }: Props) {
+  const session = await getServerSession(authOptions);
+
   const issue = await prisma?.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -23,12 +27,14 @@ async function IssueDetailpage({ params }: Props) {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 }

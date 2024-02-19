276e933a3aee3bd881fd9733e820f14491e6f983
Tunde James <tundejames04@gmail.com>
2024-02-19 12:55:36 +0100

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,22 @@ import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
 
-async function IssusesPage() {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+async function IssusesPage({ searchParams }: Props) {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>

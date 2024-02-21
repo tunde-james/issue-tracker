036873b422572f52113321bbdb2c6c68d4c72242
Tunde James <tundejames04@gmail.com>
2024-02-21 13:54:51 +0100

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -30,13 +30,22 @@ async function IssusesPage({ searchParams }: Props) {
   ];
 
   const statuses = Object.values(Status);
+
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (

58c486c2c7e2c593c78a0d745d33600c61d09f60
Tunde James <tundejames04@gmail.com>
2024-02-29 10:36:46 +0100

    Build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
 
-export default function Home() {
   return (
     <>
-      <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
     </>
   );
 }
